0cbb2c2f1bf799f1194ec2bf85641abf1ab5aadb
Sandeep <sandeepsandy958168@gmail.com>
2025-07-31 08:29:39 +0530

    Terraform Modules

---
+++ Day-07_Terraform_Modules/main.tf
@@ -0,0 +1,11 @@
+
+
+module "ec2_module" {
+
+  source = "./ec2_module" # Path Of the Module
+  ami= "ami-0b32d400456908bf9" # Ami Type (Linux/Ubuntu/Windows...)
+  instance_type = "t2.micro" # Instance Type Declaration 
+  server_name = "Module_server" # Name Of EC2 Server (tags)
+}
+
+# Here We're Calling the module by giving few required parameters (variables) like Source, AMI , Server_name, etc  
